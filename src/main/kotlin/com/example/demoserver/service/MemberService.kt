1e6c6eee5e239344d0faddc024f5f8759af0026c
unanchoi <unanc2@gmail.com>
2023-10-17 18:11:35 +0900

    비밀번호 해싱 알고리즘 추가

--- src/main/kotlin/com/example/demoserver/service/MemberService.kt
+++ src/main/kotlin/com/example/demoserver/service/MemberService.kt
@@ -5,6 +5,8 @@ import com.example.demoserver.controller.MemberController.MemberGetResponse
 import com.example.demoserver.entity.MemberEntity
 import com.example.demoserver.repository.MemberJpaRepository
 import jakarta.persistence.EntityNotFoundException
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import java.lang.IllegalArgumentException
@@ -12,7 +14,8 @@ import java.lang.IllegalArgumentException
 @Service
 @Transactional(readOnly = true)
 class MemberService(
-    private val memberJpaRepository: MemberJpaRepository
+    private val memberJpaRepository: MemberJpaRepository,
+    private val encoder: PasswordEncoder
 ) {
 
     fun getById(id: Long) : MemberGetResponse {
@@ -26,10 +29,11 @@ class MemberService(
         if (memberJpaRepository.existsByUsername(request.username)) {
             throw IllegalArgumentException("이미 존재하는 사용자입니다.")
         }
+
         val member: MemberEntity = MemberEntity(
             username = request.username,
             nickname = request.nickname,
-            password = request.password
+            password = encoder.encode(request.password)
         )
 
         val savedMember = memberJpaRepository.save(member)
@@ -39,8 +43,7 @@ class MemberService(
     fun signIn(request: MemberController.MemberSignInRequest) {
         val member: MemberEntity = memberJpaRepository.findByUsername(request.username)
             ?: throw IllegalArgumentException("사용자가 존재하지 않습니다.")
-
-        if (member.password != request.password) {
+        if (!encoder.matches(request.password, member.password)) {
             throw IllegalArgumentException("비밀번호가 일치하지 않습니다.")
         }
     }
