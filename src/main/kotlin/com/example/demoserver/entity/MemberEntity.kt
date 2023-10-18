37098759d80822b3f3b52acecaca752cc7392dfd
unanchoi <unanc2@gmail.com>
2023-10-18 19:49:59 +0900

    table 이름 수정

--- src/main/kotlin/com/example/demoserver/entity/MemberEntity.kt
+++ src/main/kotlin/com/example/demoserver/entity/MemberEntity.kt
@@ -3,9 +3,11 @@ package com.example.demoserver.entity
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.Id
+import jakarta.persistence.Table
 
 
 @Entity
+@Table(name = "member")
 class MemberEntity(
     @Id @GeneratedValue
     val id: Long? = null,
